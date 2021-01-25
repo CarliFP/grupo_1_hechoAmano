e2323c42e7433d5fdea3e48efeb33c300f660755
CarliFP <carlafreschpons@gmail.com>
2021-01-25 20:21:27 -0300

    install express y package.json

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require ("express");
+const path = require ("path");
+const app = express(); 
+
+//console.log (express)
+//console.log (path)
+//console.log (app)
+
+app.use(express.static("public"));
+
+app.listen (8000, () => {
+    console.log ("Levantando un servidor con Express: Puerto 8000")
+});
+
+app.get('/', (req, res) => {
+    //res.sendFile(path.join(__dirname, '/views/index.html'));
+    res.sendFile(path.resolve('./views/index.html'));
+    //otra forma: console.log(path.join(__dirname, '/public/hola.html'))
+    //console.log("A: "+__dirname+"/views/index.html")
+    //console.log("B: /Users/cfpons/Desktop/Liebre/views/index.html")
+});
+
+app.get('/404', (req, res) => {
+      res.send("Error página no encontrada", 404);
+    //console.log ();
+});
