00cdf1242172c67df8850ad887367d60cce59872
CarliFP <carlafreschpons@gmail.com>
2021-02-15 21:13:37 -0300

    rename view folder

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require ("express");
 const path = require ("path");
 const app = express(); 
 
+app.set("view engine", "ejs");
+
 //console.log (express)
 //console.log (path)
 //console.log (app)
