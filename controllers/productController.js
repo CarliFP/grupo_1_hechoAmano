ca7a95235c7a1440194b8819e4e0e90a2204876f
CarliFP <carlafreschpons@gmail.com>
2021-03-06 19:38:10 -0300

    routes

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,21 +3,37 @@ const path = require ("path");
 
 const productController = {
 
-    creator: (req,res) => {
+    index: (req,res) => {
+        
+    },
+
+    detail: (req,res) => {
+        
+    },
+
+    create: (req,res) => {
         res.render('productCreator');
+    },
+
+    store: (req,res) => {
+      
+    },
+
+    edit: (req,res) => {
+        
+    },
+    update: (req,res) => {
+       
+    },
+    destroy: (req,res) => {
+       
     },
 
     //cart:(req,res) => {
         //res.render('productCart');
     //},
     
-    detail:(req,res) => {
-        res.render('productDetail');
-    },
 
-    detalle:(req,res) => {
-        res.send("Bienvenidos al detalle del producto " + req.params.idProduct);
-    }
 }
 
 module.exports = productController;
--- routes/product.js
+++ routes/product.js
@@ -1,14 +1,55 @@
-const express = require ("express");
-const productController = require ('../controllers/productController');
-const path = require ("path");
-const router = express.Router ();
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
+const path = require('path'); 
+const multer = require('multer'); 
 
-router.get('/productCreator', productController.creator);
+// ************ Controller Require ************
+const productController = require('../controllers/productController');
+
+//**************** MULTER **********************************//
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, './public/images/products');
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);
+    }
+}); 
+
+// Listado de productos
+router.get('/', productController.index); 
+
+//Detalle de un producto particular
+router.get('/:id/detail', productController.detail); 
+
+//Formulario de creación de productos
+router.get('/create', productController.create); 
+
+//Acción de creación (a donde se envía el formulario)
+const uploadFile = multer({storage: storage}); //aquí se almacena la ejecución
+
+//router.post('/', uploadFile.single('image'), productsController.store);
+
+//en el medio va el nombre name dado en el formulario 
+
+
+//Formulario de edición de productos
+router.get('/:id/edit', productController.edit); // puede ser edith/id cuidado de no solaparse
+
+//Acción de edición (a donde se envía el formulario):
+router.put('/:id', productController.update); 
+
+//Acción de borrado
+router.delete('/:id/delete', productController.destroy); 
+
+
+//router.get('/productCreator', productController.creator);
 
 //router.get('/productCart', productController.cart);
 
-router.get('/productDetail', productController.detail);
+//router.get('/productDetail', productController.detail);
 
-router.get('/:idProduct', productController.detalle);
+//router.get('/:idProduct', productController.detalle);
 
 module.exports = router;
