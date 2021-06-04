c592ab2c0ecfc3300aa798dec81ea1ef8ab29ab5
Neri <neri1412@hotmail.com>
2021-06-04 20:02:52 -0300

    Validando register Front

--- public/js/userCreateValidator.js
+++ public/js/userCreateValidator.js
@@ -2,9 +2,35 @@ window.onload = function(){
 
     let form = document.getElementById("form")
 
-    form.addEventListener("submit", function(){
+    let name = document.querySelector(".name")
+    let user = document.querySelector(".user")
+    let email = document.querySelector(".email")
+    let birthDate = document.querySelector(".birthDate")
+    let adress = document.querySelector(".adress")
+    let country = document.querySelector(".country")
+    //: let user = falta capturar select
+    //: let profile = falta capturar radio 
+    //: let interesets = falta capturar checkbox
+    let avatar =  document.querySelector(".avatar")
+    let password = document.querySelector(".password-input")
+
+    let resultado = true; //Esto validará si los campos están correctos, en caso de que alguno de los campos NO cumpla con el requisito esto pasará a False.
+
+    name.addEventListener('focusout',function(){    
+        if (this.value.length > 2) {
+        console.log("Cumple!!")
+    }else{
         event.preventDefault();
-        alert("usuario no creado");
+        this.style.display = "block";
+    }});
+
+    form.addEventListener("submit", function(){
+        if (resultado === false){
+            event.preventDefault();
+            alert("Por favor, revise todos los campos.")
+        }else{
+
+        }
     })
 
 }
