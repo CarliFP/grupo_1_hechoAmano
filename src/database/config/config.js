0e2763f3fca173697483d963c4203f44ac577ab7
NeriCardozo <75796427+NeriCardozo@users.noreply.github.com>
2021-06-09 21:16:19 -0300

    Merge

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": 'root',
-    "database": "HechoAmanoDB",
+    "password": "",
+    "database": "structureconproductos",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
