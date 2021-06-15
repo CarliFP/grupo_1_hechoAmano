5b6301c0491d51534b38dc8bcc7db0b2f6a9b89b
NeriCardozo <75796427+NeriCardozo@users.noreply.github.com>
2021-06-14 22:13:05 -0300

    Terminando de validar con los js

--- public/js/productEditAdmin.js
+++ public/js/productEditAdmin.js
@@ -1,15 +1,69 @@
 window.onload = function(){
 
-    let adminCheck = document.querySelector(".adminCheck")
+    let email = document.querySelector(".email")
+    let comment = document.querySelector(".questionBox")
     let adminOnly = document.getElementById("adminOnly")
+    let eraseButton = document.querySelector(".submit-button")
 
-    let estadoSecreto = 0;
+    let form = document.querySelector(".questionField-form")
 
-    adminCheck.addEventListener('mouseout', () => {
-        adminOnly.classList.add("display:block");
+
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    email.addEventListener('focusout',function(){   
+        if (re.test(email.value)) {
+        var errorDiv = document.querySelector(".emailErrorDiv")
+        errorDiv.style.display = "none"
+    }else{
+        var errorDiv = document.querySelector(".emailErrorDiv")
+        errorDiv.style.display = "block";
+        errorDiv.style.color = "tomato";
+        errorDiv.innerHTML = "No soy un mail!"
+    }});
+
+    comment.addEventListener('focusout',function(){               
+        if (comment.value.length > 0) {
+        var errorDiv = document.querySelector(".commentErrorDiv")
+        errorDiv.style.display = "none";
+    }else{
+        var errorDiv = document.querySelector(".commentErrorDiv")
+        errorDiv.style.display = "block";
+        errorDiv.style.color = "tomato";
+        errorDiv.innerHTML = "No puedo estar vacio!!"
+    }
     })
 
-    adminCheck.addEventListener('keypress',function(e){
+    form.addEventListener('submit', e=>{
+        e.preventDefault();
+        if (re.test(email.value) && comment.value.length > 0){
+            form.submit();
+
+            var errorDiv = document.querySelector(".formErrorDiv")
+            errorDiv.style.display = "none"
+        }else{
+            var errorDiv = document.querySelector(".formErrorDiv")
+            errorDiv.style.display = "block";
+            errorDiv.style.color = "tomato";
+            errorDiv.innerHTML = "Por favor, complete los campos."
+        }
+    })
+
+    eraseButton.addEventListener('click', function(){
+        email.value = "";
+        comment.value = "";
+        var errorDiv1 = document.querySelector(".emailErrorDiv")
+        var errorDiv2 = document.querySelector(".commentErrorDiv")
+        var errorDiv3 = document.querySelector(".formErrorDiv")
+
+        errorDiv1.style.display = "none"
+        errorDiv2.style.display = "none"
+        errorDiv3.style.display = "none"
+
+    })
+
+    
+    let estadoSecreto = 0;
+    window.addEventListener('keypress',function(e){
         switch (estadoSecreto) {
             case 0:
                 e.key == "s" ? estadoSecreto++ : (estadoSecreto = 0) ;
