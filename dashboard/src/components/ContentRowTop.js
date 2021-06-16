5106a8f8403019fd6b7fdaba48da163a86a1ce9c
NeriCardozo <75796427+NeriCardozo@users.noreply.github.com>
2021-06-15 23:36:20 -0300

    Agregando dashboard

--- dashboard/src/components/ContentRowTop.js
+++ dashboard/src/components/ContentRowTop.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import imagenFondo from '../assets/images/pintura.png'
+
 function ContentRowTop(){
+	const urlProduct = "/api/product"
+	const urlUsers = "/api/users"
+	const [products,setProducts] = useState()
+	const [users,setUsers] = useState()
+	let [lastProduct, setLastProduct] = useState() 
+
+const fetchApi =  async () => {
+	const response1= await fetch(urlProduct);
+	const response1JSON = await response1.json();
+	setProducts(response1JSON);
+	const response2= await fetch(urlUsers);
+	const response2JSON = await response2.json();
+	setUsers(response2JSON);
+	let id = (response1JSON.data.length - 1)
+	let lastProductTest = response1JSON.data[id]
+	console.log(lastProductTest);
+	setLastProduct(response1JSON.data[id]);
+}
+useEffect(() => {
+	fetchApi();
+},
+
+
+[])
+	
     return(
         <React.Fragment>
 				{/*<!-- Content Row Top -->*/}
@@ -19,7 +45,7 @@ function ContentRowTop(){
 									<div className="row no-gutters align-items-center">
 										<div className="col mr-2">
 											<div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Total de Productos</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">XXXXX</div>
+											<div className="h5 mb-0 font-weight-bold text-gray-800">{ !products ? 'Cargando...' : products.data.length}</div>
 										</div>
 			
 									</div>
@@ -34,7 +60,7 @@ function ContentRowTop(){
 									<div className="row no-gutters align-items-center">
 										<div className="col mr-2">
 											<div className="text-xs font-weight-bold text-success text-uppercase mb-1">Total de Usuarios</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">XXX</div>
+											<div className="h5 mb-0 font-weight-bold text-gray-800">{!users ? 'Cargando...' : users.data.length}</div>
 										</div>
 									
 									</div>
@@ -50,7 +76,7 @@ function ContentRowTop(){
 										<div className="col mr-2">
 											<div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Total de Categorías
 											</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">XXX</div>
+											<div className="h5 mb-0 font-weight-bold text-gray-800">6</div>
 										</div>
 						
 									</div>
@@ -70,10 +96,8 @@ function ContentRowTop(){
 									<h5 className="m-0 font-weight-bold text-gray-800">Último producto creado</h5>
 								</div>
 								<div className="card-body">
-									<div className="text-center">
-										<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={imagenFondo} alt="  "/>
-									</div>
-									<p>Acá poner el detalle</p>
+									<p>{!lastProduct ? 'Cargando...' : lastProduct.name}</p>
+									<p>{!lastProduct ? 'Cargando...' : lastProduct.description}</p>
 									<a className="btn btn-danger" target="_blank" rel="nofollow" href="/">Ver detalle de Producto</a>
 								</div>
 							</div>
@@ -81,7 +105,8 @@ function ContentRowTop(){
 						{/*<!-- End content row last movie in Data Base -->*/}
 
 						{/*<!-- Genres in DB -->*/}
-						<div className="col-lg-6 mb-4">						
+						
+				<div className="col-lg-6 mb-4">						
 							<div className="card shadow mb-4">
 								<div className="card-header py-3">
 									<h5 className="m-0 font-weight-bold text-gray-800">Categorias</h5>
@@ -130,15 +155,35 @@ function ContentRowTop(){
 												</div>
 											</div>
 										</div>
+										
 
 									</div>
+
+									
 								</div>
 							</div>
+						</div>	
+						<div className="col-lg-6 mb-4">						
+							<div className="card shadow mb-4">
+								<div className="card-header py-3">
+									<h5 className="m-0 font-weight-bold text-gray-800">Productos</h5> 
+							</div>
+								<div className="card-body">
+								<div className="row">
+												{!products ? 'Cargando...' : products.data.map((products,index)=> {
+													return <div className="col-lg-6 mb-4">
+														<div className="card bg-dark text-white shadow">
+														<div className="card-body">{products.name}</div>
+													</div>
+									</div>})}
+								</div>		
+							</div>	
+							
 						</div>
-					</div>
-				</div>
-				{/*<!--End Content Row Top-->*/}
 
+					</div>
+				</div>		
+</div>
         </React.Fragment>
     )
 
